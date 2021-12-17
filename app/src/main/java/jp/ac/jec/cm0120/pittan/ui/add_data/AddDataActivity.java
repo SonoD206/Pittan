541db2531da798e5c9b871336e9e698a5de087b6
SonoD206 <20cm0120@jec.ac.jp>
2021-12-17 11:01:23 +0900

    ユーザーが入力したコメントをLocalDBに保存

--- app/src/main/java/jp/ac/jec/cm0120/pittan/ui/add_data/AddDataActivity.java
+++ app/src/main/java/jp/ac/jec/cm0120/pittan/ui/add_data/AddDataActivity.java
@@ -3,7 +3,6 @@ package jp.ac.jec.cm0120.pittan.ui.add_data;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -28,18 +27,16 @@ public class AddDataActivity extends AppCompatActivity {
 
   private static final String TAG = "###";
   private Intent intent;
-  
+
   /// Components
   private MaterialButtonToggleGroup segmentedControl;
   private FrameLayout frameLayout;
   private MaterialToolbar mToolbar;
-  private Button buttonCurtain;
-  private Button buttonRug;
   private TextInputEditText editLocation;
   private TextInputEditText editHeightSize;
   private TextInputEditText editWidthSize;
   private TextInputEditText editComments;
-  
+
   ///DBItem
   private int placeDeleteFlag = 0;
   private float productHeight = 0f;
@@ -63,8 +60,8 @@ public class AddDataActivity extends AppCompatActivity {
   private void initialize() {
     mToolbar = findViewById(R.id.add_top_bar);
     frameLayout = findViewById(R.id.frame_photo);
-    buttonCurtain = findViewById(R.id.button_curtain);
-    buttonRug = findViewById(R.id.button_rug);
+    Button buttonCurtain = findViewById(R.id.button_curtain);
+    Button buttonRug = findViewById(R.id.button_rug);
     segmentedControl = findViewById(R.id.segmented_controller);
     editLocation = findViewById(R.id.edit_view_location);
     editHeightSize = findViewById(R.id.edit_view_size_height);
@@ -102,7 +99,7 @@ public class AddDataActivity extends AppCompatActivity {
 
     /// PhotoFrame
     frameLayout.setOnClickListener(view -> {
-      intent = new Intent(this,ObjectInstallationActivity.class);
+      intent = new Intent(this, ObjectInstallationActivity.class);
       startForResult.launch(intent);
     });
 
@@ -129,7 +126,7 @@ public class AddDataActivity extends AppCompatActivity {
   public boolean onOptionsItemSelected(@NonNull MenuItem item) {
     if (item.getItemId() == R.id.menu_item_save_data) {
       // TODO: 2021/12/10  PittanSQLiteOpenHelperのInsert実行　鬼門は画像保存
-      
+
       if (editHeightSize.getText().toString().length() != 0) {
         productHeight = Float.parseFloat(editHeightSize.getText().toString());
       }
@@ -158,7 +155,7 @@ public class AddDataActivity extends AppCompatActivity {
     /// placeテーブル
     model.setPlaceName(editLocation.getText().toString());
     model.setPlaceDeleteFlag(placeDeleteFlag);
-    
+
     ///productテーブル
     model.setProductHeight(productHeight);
     model.setProductWidth(productWidth);
@@ -166,6 +163,7 @@ public class AddDataActivity extends AppCompatActivity {
     model.setProductColorCode(productColorCode);
     model.setProductDesign(productDesign);
     model.setProductType(productType);
+    model.setProductComment(editComments.getText().toString());
 
     ///productImageテーブル
     model.setProductImagePath(productImagePath);
