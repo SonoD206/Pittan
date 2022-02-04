bfa71659d9d1c126e7abad8ec7818f5997426f85
SonoD206 <20cm0120@jec.ac.jp>
2022-02-04 17:30:12 +0900

    AppConstantの編集

--- app/src/main/java/jp/ac/jec/cm0120/pittan/app/AppConstant.java
+++ app/src/main/java/jp/ac/jec/cm0120/pittan/app/AppConstant.java
@@ -1,9 +1,15 @@
 package jp.ac.jec.cm0120.pittan.app;
 
 public final class AppConstant {
-  private AppConstant() {}
+  private AppConstant() {
+  }
 
   public static final String EXTRA_TRANSITION_NAME = "TransitionName";
+
+  public static class Log {
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+  }
 
   public static class Home {
     public static final String EXTRA_PLACE_ID = "placeID";
@@ -16,9 +22,9 @@ public final class AppConstant {
   }
 
   public static class Objection {
-
     public static final int MODEL_NUM = 1;
     public static final int TEXTURE_NUM = 2;
+    public static final int FRAGMENT_PAGE_NUM = 2;
     public static final String ACTIVITY_NAME = "Objection";
     public static final String EXTRA_IMAGE_TEMP_FILE_PATH = "imageTempPath";
     public static final String EXTRA_IMAGE_FILE_PATH = "imagePath";
@@ -27,11 +33,12 @@ public final class AppConstant {
     public static final String BASE_COLOR_MAP = "baseColorMap";
     public static final String HANDLER_THREAD_NAME = "PixelCopier";
     public static final String DATE_FORMAT_PATTERN = "yyyy_MM_dd_HHmm";
-    public static final String TEMP_PICTURE_NAME ="temp.jpg";
-    public static final String ALERT_MESSAGE_FORMAT =  "縦幅：%smm\n" + "横幅：%smm" ;
+    public static final String TEMP_PICTURE_NAME = "temp.jpg";
+    public static final String ALERT_MESSAGE_FORMAT = "縦幅：%smm\n" + "横幅：%smm";
     public static final String USER_PICTURE_NAME_FORMAT_PATTERN = "Pittan/%s_3dModel.jpg";
     public static final String MODELS_PATH_FORMAT_PATTERN = "models/%s";
     public static final String TEXTURES_PATH_FORMAT_PATTERN = "textures/%s";
-
+    public static final String ASSETS_FILE_NAME = "textures";
+    public static final String DATA_SIZE_FORMAT = "%smm";
   }
 }
