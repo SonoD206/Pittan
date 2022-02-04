44e98a1d0ab0fc99c265a5f1b2c62c11d69a0d8f
SonoD206 <20cm0120@jec.ac.jp>
2022-02-04 12:29:11 +0900

    AppConstant.javaの編集

--- app/src/main/java/jp/ac/jec/cm0120/pittan/app/AppConstant.java
+++ app/src/main/java/jp/ac/jec/cm0120/pittan/app/AppConstant.java
@@ -12,10 +12,14 @@ public final class AppConstant {
 
   public static class Detail {
     public static final String EXTRA_MODEL = "PittanProductDataModel";
+    public static final String ACTIVITY_NAME = "Detail";
   }
+
   public static class Objection {
+
     public static final int MODEL_NUM = 1;
     public static final int TEXTURE_NUM = 2;
+    public static final String ACTIVITY_NAME = "Objection";
     public static final String EXTRA_IMAGE_TEMP_FILE_PATH = "imageTempPath";
     public static final String EXTRA_IMAGE_FILE_PATH = "imagePath";
     public static final String FIRST_MODEL = "white_curtain.glb";
@@ -23,11 +27,11 @@ public final class AppConstant {
     public static final String BASE_COLOR_MAP = "baseColorMap";
     public static final String HANDLER_THREAD_NAME = "PixelCopier";
     public static final String DATE_FORMAT_PATTERN = "yyyy_MM_dd_HHmm";
-    public static final String TRANSITION_NAME_OBJECT = "Object";
-    public static final String ALERT_MESSAGE_FORMAT =  "縦幅：%smm\n" + "横幅：%smm" ;
     public static final String TEMP_PICTURE_NAME ="temp.jpg";
+    public static final String ALERT_MESSAGE_FORMAT =  "縦幅：%smm\n" + "横幅：%smm" ;
     public static final String USER_PICTURE_NAME_FORMAT_PATTERN = "Pittan/%s_3dModel.jpg";
     public static final String MODELS_PATH_FORMAT_PATTERN = "models/%s";
     public static final String TEXTURES_PATH_FORMAT_PATTERN = "textures/%s";
+
   }
 }
